353c2b96696be6f3edb47f7b27aac6d257ac00e2
NayanaRaghavaShet5 <51402931+NayanaRaghavaShet5@users.noreply.github.com>
2020-06-06 18:13:25 +0530

    cc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    first: "",
+    last: ""
+  });
+
+  function handleName(e) {
+    const { value, name } = e.target;
+    console.log(value);
+    setFullName(preValue => {
+      if (name === "fName") {
+        return { first: value, last: preValue.last };
+      } else if (name === "lName") {
+        return {
+          first: preValue.first,
+          last: value
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.first} {fullName.last}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={handleName}
+          name="fName"
+          placeholder="First Name"
+          value={fullName.first}
+        />
+        <input
+          onChange={handleName}
+          name="lName"
+          placeholder="Last Name"
+          value={fullName.last}
+        />
         <button>Submit</button>
       </form>
     </div>
